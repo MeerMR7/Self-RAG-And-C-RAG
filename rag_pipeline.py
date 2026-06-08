bf13422f3c9cd254439dcd58479888e147a373fa
Mir Muhammad Rafique <muhammadmeer271@gmail.com>
2026-06-08 02:46:55 -0700

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -6,7 +6,7 @@ from functools import lru_cache
 from langchain_groq import ChatGroq
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.vectorstores import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
